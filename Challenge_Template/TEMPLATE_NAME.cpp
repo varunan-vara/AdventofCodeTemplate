899b212373b05a28514fcbc9834fc839c72bf80a
Varunan Varathan <varatv3@mcmaster.ca>
2025-11-27 13:24:18 -0500

    Added to cpp

--- Challenge_Template/TEMPLATE_NAME.cpp
+++ Challenge_Template/TEMPLATE_NAME.cpp
@@ -1,2 +1,9 @@
 #include "../Utils/AoCUtils.h"
+#include <iostream>
 
+int main (int argc, char *argv[]) {
+    // Insert your main AOC Project Code here
+    // 
+    // 
+    std::cout << "Challenge code for " << argv[0] << std::endl;
+}
